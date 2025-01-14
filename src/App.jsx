0449a79f01753963c49689ec0d50c0187d5a8530
Ali Ashooriyoon <72642763+AliAshooriyoon@users.noreply.github.com>
2025-01-14 21:00:42 +0330

    add Background

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./components/pages/Home/Home";
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <Home />
+      </div>
+    </>
+  );
+};
+
+export default App;
