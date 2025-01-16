87b71055bbb427b1c45031d4df6a5c62e0d45cb9
Ali Ashooriyoon <72642763+AliAshooriyoon@users.noreply.github.com>
2025-01-17 00:55:32 +0330

    develop and styling Squares

--- src/components/pages/GameMain/GameMain.jsx
+++ src/components/pages/GameMain/GameMain.jsx
@@ -1,6 +1,7 @@
 import "./GameMain.css";
 import Name from "../Name/Name.jsx";
 import { useState } from "react";
+import SquaresBox from "../SquaresBox/SquaresBox.jsx";
 const GameMain = () => {
   const [user1, setUser1] = useState("Ali");
   const [user2, setUser2] = useState("Ahura");
@@ -14,6 +15,7 @@ const GameMain = () => {
           <Name name={user1} userID={1} submitId={submitName} />
           <Name name={user2} userID={2} submitId={submitName} />
         </div>
+        <SquaresBox />
       </div>
     </>
   );
---
+++ src/components/pages/SquaresBox/SquaresBox.jsx
@@ -0,0 +1,20 @@
+import Square from "../Square/Square";
+import "./SquaresBox.css";
+const SquaresBox = () => {
+  return (
+    <>
+      <div className="SquaresBox">
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+};
+export default SquaresBox;
