c93f8dd27f6b24368fff6d9748711f284c6e8beb
Ali Ashooriyoon <72642763+AliAshooriyoon@users.noreply.github.com>
2025-01-16 19:40:02 +0330

    develop names part

--- src/components/pages/GameMain/GameMain.jsx
+++ src/components/pages/GameMain/GameMain.jsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 const GameMain = () => {
   const [user1, setUser1] = useState("Ali");
   const [user2, setUser2] = useState("Ahura");
+  const submitName = (input, userID) => {
+    userID == 1 ? setUser1(input) : setUser2(input);
+  };
   return (
     <>
       <div className="gameMain">
         <div className="names">
-          <Name name={user1} />
-          <Name name={user2} />
+          <Name name={user1} userID={1} submitId={submitName} />
+          <Name name={user2} userID={2} submitId={submitName} />
         </div>
       </div>
     </>
--- src/components/pages/Name/Name.jsx
+++ src/components/pages/Name/Name.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 import "./Name.css";
 // eslint-disable-next-line react/prop-types
-const Name = ({ name }) => {
+const Name = ({ name, submitId, userID }) => {
   const [changeTypeOfElm, setChangeTypeOfElm] = useState(false);
+  const [newName, setNewName] = useState("");
+  // const setNewNameFunc = () => {};
   const editName = () => {
     console.log("Hi");
     setChangeTypeOfElm(!changeTypeOfElm);
-    console.log(changeTypeOfElm);
+    // console.log(changeTypeOfElm);
+    changeTypeOfElm && submitId(newName, userID);
   };
   return (
     <>
       <div className="name">
-        {!changeTypeOfElm ? <p>{name}</p> : <input type="text" name="" />}
+        {!changeTypeOfElm ? (
+          <p>{name}</p>
+        ) : (
+          <input
+            type="text"
+            name=""
+            className="input_text"
+            placeholder={name}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        )}
         <button type="button" className="btn-usr" onClick={editName}>
           Verändern
         </button>
